c8d226ced4be5313a4f067838ba701d8c26b451a
Alpuds <sduanuf@gmail.com>
2021-05-06 14:14:04 -0700

    recovered accidental deletion

--- games/blackjack.py
+++ games/blackjack.py
@@ -2,6 +2,6 @@ def expand_gold(amount):
     """Exapands the abbreviated amount of gold into an integer (e.g. 1.5K -> 1,500)"""
 
 
-def blackjack(bet_amount):  
+def blackjack(bet_amount):
     """This accepts a bet wich can be any number besides 0"""
     print(f"You have {bet_amount} as your bet.")
