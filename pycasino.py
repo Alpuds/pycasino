f75f1f20f63e1e40b148ec7b48845596dde56515
Alpuds <sduanuf@gmail.com>
2021-05-08 12:46:35 -0700

    made it display the amount of gold logically as well as csv implementation

--- pycasino.py
+++ pycasino.py
@@ -3,6 +3,8 @@ Pycasino is a casino in which it uses gold as currency. Gold can be obtained thr
 Of course, in order to play said games, gold needs to be put in as the bet. In addition, stats for each game is available.
 This consists of times played, total win/loss and net gain of gold.
 """
+import csv
+
 from games import blackjack as bj
 
 def title():
@@ -13,12 +15,26 @@ def title():
     print("██║        ██║   ╚██████╗██║  ██║███████║██║██║ ╚████║╚██████╔╝".center(100, "-"))
     print("╚═╝        ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝".center(98, "-"))
 
+<<<<<<< HEAD
 gold = 1_665_390
+=======
+username = "Alpud"
+data_path = "./users/" + username + "/data.csv"
+>>>>>>> user_csv_handling
 
 def list_of_commands():
     with open("help.txt", "r") as f:
        return f.read()
 
+<<<<<<< HEAD
+=======
+def read_data(field):
+    with open(data_path, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for line in csv_reader:
+            return line[field]
+
+>>>>>>> user_csv_handling
 def abbrv(num):
     """Shortens the amount so it will have a letter at the end to indicate the place value of the number (e.g. 1.5K = 1,500)
        This goes upto trillion.
@@ -45,7 +61,12 @@ while True:
 
     except ValueError:
         if command == "gold":
+<<<<<<< HEAD
             print(f"You have {gold:,} ({abbrv(gold)}) gold.")
+=======
+            gold = int(read_data("gold"))
+            print(f"You have {gold:,} ({abbrv(gold)}) gold." if gold > 1_000 else f"You have {gold} gold.")
+>>>>>>> user_csv_handling
 
         elif command == "q" or command == "Q":
             break
