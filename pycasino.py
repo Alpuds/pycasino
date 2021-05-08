1002e64b80452f5f495018ba41808e63d66105c1
Alpuds <sduanuf@gmail.com>
2021-05-08 12:58:16 -0700

    fixed residue from merge conflict

--- pycasino.py
+++ pycasino.py
@@ -15,26 +15,19 @@ def title():
     print("██║        ██║   ╚██████╗██║  ██║███████║██║██║ ╚████║╚██████╔╝".center(100, "-"))
     print("╚═╝        ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝".center(98, "-"))
 
-<<<<<<< HEAD
-gold = 1_665_390
-=======
 username = "Alpud"
 data_path = "./users/" + username + "/data.csv"
->>>>>>> user_csv_handling
 
 def list_of_commands():
     with open("help.txt", "r") as f:
        return f.read()
 
-<<<<<<< HEAD
-=======
 def read_data(field):
     with open(data_path, "r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for line in csv_reader:
             return line[field]
 
->>>>>>> user_csv_handling
 def abbrv(num):
     """Shortens the amount so it will have a letter at the end to indicate the place value of the number (e.g. 1.5K = 1,500)
        This goes upto trillion.
@@ -61,12 +54,8 @@ while True:
 
     except ValueError:
         if command == "gold":
-<<<<<<< HEAD
-            print(f"You have {gold:,} ({abbrv(gold)}) gold.")
-=======
             gold = int(read_data("gold"))
             print(f"You have {gold:,} ({abbrv(gold)}) gold." if gold > 1_000 else f"You have {gold} gold.")
->>>>>>> user_csv_handling
 
         elif command == "q" or command == "Q":
             break
