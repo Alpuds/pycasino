1858830fcd1382140bc0d25b1f2263e1c33b431e
Alpuds <sduanuf@gmail.com>
2021-05-12 12:58:21 -0700

    added "write_data" function as well as the ability to add gold

--- pycasino.py
+++ pycasino.py
@@ -4,6 +4,7 @@ Of course, in order to play said games, gold needs to be put in as the bet. In a
 This consists of times played, total win/loss and net gain of gold.
 """
 import csv
+import os
 
 from games.blackjack import blackjack
 from games.craps import craps
@@ -52,9 +53,37 @@ def read_data(field, game=0, stats=0):
                     return int(line[stats])
 
 
+def write_data(field, write, game=0, stat=0):
+    """
+    Writes data to the data.csv. It can update gold or a game's specific statistic (e.g. wins).
+    """
+    with open('data.csv', 'r') as csv_file, open('updated_data.csv', 'w') as f:
+        field_names = ('gold', 'game', 'wins', 'losses', 'total gold gained', 'total gold lost', 'gold net value')
+
+        csv_writer = csv.DictWriter(f, fieldnames=field_names)
+        csv_reader = csv.DictReader(csv_file)
+
+        csv_writer.writeheader()
+
+        for line in csv_reader:
+            if game != 0:
+                if line[field] == game:
+                    line[stat] = write
+            # Relies on "gold" being the first field name
+            elif line[field] != write and line[field] != 'x':
+                line[field] = write
+            else:
+                line[field] = 'x'
+            # Updates data
+            row = {'gold': line['gold'], 'game': line['game'], 'wins': line['wins'], 'losses': line['losses'],'total gold gained': line['total gold gained'], 'total gold lost': line['total gold lost'], 'gold net value': line['gold net value'] }
+            csv_writer.writerow(row)
+
+        os.rename('updated_data.csv', 'data.csv')
+
+
 def abbrv(num):
     """
-    Shortens the amount so it will have a letter at the end to indicate the place value of the number (e.g. 1.5K = 1,500)
+    Shortens the amount so it will have a letter at the end to indicate the place value of the number (e.g. 1,500 -> 1.5K)
     This goes upto trillion.
     """
     abbrv = {"T": 1_000_000_000_000, "B": 1_000_000_000, "M": 1_000_000, "K": 1000}
@@ -97,6 +126,7 @@ while True:
     try:
         command = input("Command: ")
         command, bet = command.split()
+        #---Stats----#
         if command == "stats":
             if bet in games:
                 if bet == "bj":
@@ -106,8 +136,9 @@ while True:
                 print(
                     f'{bet.capitalize()} is not a game available. Type "h" or "help" to see a list of commands'
                 )
-        # Checks the bet and expands it when needed.
+        #------------#
         elif command in games:
+            #---Checks the integrity of the bet and expands it when needed---#
             if check_abbrv(bet) == True:
                 bet = expand(bet)
             elif check_abbrv(bet) == "invalid":
@@ -115,12 +146,22 @@ while True:
                 continue
             else:
                 bet = int(bet)
-
+            #----------------------------------------------------------------#
             if bet > gold:
                 print(f"You don't have enough gold. [{gold:,}] ({abbrv(gold)})")
                 continue
+            #---Game---#
             games[command](bet)
             continue
+            #----------#
+        #--Add gold when allowed---#
+        elif command == 'add' and bet == 'gold':
+            if gold == 0:
+                write_data('gold', 100)
+                print('Added 100 gold.')
+            else:
+                print('You can only add gold if you ran out of it.')
+        #--------------------------#
         else:
             print('Invalid command. Type "h" or "help" for a list of commands')
             continue
@@ -139,12 +180,16 @@ while True:
         elif command == "h" or command == "help":
             print(list_of_commands())
 
+        #---Zero argument error handling---#
         elif command in games:
              print("You need to provide a bet.")
 
         elif command == "stats":
             print("You need to provide a game to get stats.")
 
+        elif command == "add":
+            print('Type "add gold" to receive 100 gold if and only if you ran out of gold.')
+        #----------------------------------#
         else:
             print('Invalid command. Type "h" or "help" for a list of commands')
             continue
